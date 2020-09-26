f50120b15eda7b7010c148523e3613468faffc90
Shotaro Tanaka <yubessy0@gmail.com>
2020-09-26 13:22:07 +0900

    resize according to window.innerWidth (#2)

---
+++ precache-manifest.f9a392f45d1d06a411bd6ad51d909493.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56e0d5fc118bb1ca3644fffcfbd96ea7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "254104526d33cc35dc39",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d0bffaeea8fcedcab0f2",
+    "url": "/static/js/2.906a5a24.chunk.js"
+  },
+  {
+    "revision": "a13369e7405157f254f1ffcc4cc651e5",
+    "url": "/static/js/2.906a5a24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "254104526d33cc35dc39",
+    "url": "/static/js/main.5698d139.chunk.js"
+  },
+  {
+    "revision": "16f3884a2b5d6f9804e7",
+    "url": "/static/js/runtime-main.df8ff79f.js"
+  }
+]);
