2525d3f1dcd18271ccd19dd047c385cd92f047fd
Shotaro Tanaka <yubessy0@gmail.com>
2020-09-27 23:28:23 +0900

    prevent double tap zoom again (#6)

---
+++ precache-manifest.384ea376a379fe1a3125dade10d2588c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e19065778e4ae588516324737fbff26f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92c900736a6df967e58a",
+    "url": "/static/css/main.bee81c70.chunk.css"
+  },
+  {
+    "revision": "b13fc913cf0341eb76cb",
+    "url": "/static/js/2.f37313ca.chunk.js"
+  },
+  {
+    "revision": "a13369e7405157f254f1ffcc4cc651e5",
+    "url": "/static/js/2.f37313ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92c900736a6df967e58a",
+    "url": "/static/js/main.5cd0e3f0.chunk.js"
+  },
+  {
+    "revision": "16f3884a2b5d6f9804e7",
+    "url": "/static/js/runtime-main.df8ff79f.js"
+  }
+]);
